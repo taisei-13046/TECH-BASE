d5f3db69fdea9790c5c1a2aee265d212e17f1645
taisei-13046 <78260526+taisei-13046@users.noreply.github.com>
2021-07-06 21:29:24 +0900

    Update 4-2.php

--- TECH＿BASE/public_html/mission4/4-2.php
+++ TECH＿BASE/public_html/mission4/4-2.php
@@ -2,7 +2,7 @@
 	echo "helo<br>";
 	$dsn = 'mysql:dbname=tb230001db;host=localhost';
     $user = 'tb-230001';
-    $password = 'L66myFn9hG';
+    $password = 
     $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 	$sql = "CREATE TABLE IF NOT EXISTS tbtest (
 		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
