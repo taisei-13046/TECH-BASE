740df70b32ed951253acc2a54e10ea666f479c8e
taisei-13046 <78260526+taisei-13046@users.noreply.github.com>
2021-07-06 21:30:52 +0900

    Update 4-9.php

--- TECH＿BASE/public_html/mission4/4-9.php
+++ TECH＿BASE/public_html/mission4/4-9.php
@@ -1,7 +1,7 @@
 <?php
 	$dsn = 'mysql:dbname=tb230001db;host=localhost';
     $user = 'tb-230001';
-    $password = 'L66myFn9hG';
+    $password = 
     $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 	$sql = 'DROP TABLE tbtest';
 	$stmt = $pdo->query($sql);
