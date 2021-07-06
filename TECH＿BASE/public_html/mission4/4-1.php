6e4d8afcda7b7bf84bb50696716f884be2e7b053
taisei-13046 <78260526+taisei-13046@users.noreply.github.com>
2021-07-06 21:29:09 +0900

    Update 4-1.php

--- TECH＿BASE/public_html/mission4/4-1.php
+++ TECH＿BASE/public_html/mission4/4-1.php
@@ -2,7 +2,7 @@
 	echo "helo<br>";
 	$dsn = 'mysql:dbname=tb230001db;host=localhost';
     $user = 'tb-230001';
-    $password = 'L66myFn9hG';
+    $password = 
     $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 	echo "helo<br>";
 ?>
